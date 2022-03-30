3cb5be91260f03caf30d17e37c8280be06a9371d
code-x22 <wongwongansawah2021@gmail.com>
2022-03-30 21:18:33 +0700

    senton

---
+++ KIR5/listmenu.js
@@ -0,0 +1,573 @@
+exports.ownermenu = (prefix) => 
+{
+return `⌜ 𝐎𝐖𝐍𝐄𝐑 𝐌𝐄𝐍𝐔 ⌝
+メ >
+メ ${prefix}public
+メ ${prefix}self
+メ ${prefix}leave
+メ ${prefix}term
+メ ${prefix}bc *teks*
+メ ${prefix}bcg *teks*
+メ ${prefix}join *linkgc*
+メ ${prefix}addprem *@tag*
+メ ${prefix}delprem *@tag*
+メ ${prefix}listpremium
+メ ${prefix}ban *@tag*
+メ ${prefix}unban *@tag*
+メ ${prefix}setnamabot *nama*
+メ ${prefix}setbioo *bio*
+メ ${prefix}setppbot *reply foto*`
+}
+exports.othermenu = (prefix) => 
+{
+return `⌜ 𝐎𝐓𝐇𝐄𝐑 𝐌𝐄𝐍𝐔 ⌝
+メ ${prefix}tes
+メ ${prefix}runtime
+メ ${prefix}owner
+メ ${prefix}donasi
+メ ${prefix}sewabot
+メ ${prefix}ytowner
+メ ${prefix}igowner
+メ ${prefix}ttowner
+メ ${prefix}gtowner
+メ ${prefix}gckirbotz
+メ ${prefix}anime *nama anime*
+メ ${prefix}kusonime *nama anime*
+メ ${prefix}cekserver *hostname*
+メ ${prefix}tololserti *teks*
+メ ${prefix}fuckboy *teks*
+メ ${prefix}fuckgirl *teks*
+メ ${prefix}bucinserti *teks*
+メ ${prefix}pacar *teks teks*
+メ ${prefix}serti1 *teks*
+メ ${prefix}serti2 *teks*
+メ ${prefix}serti3 *teks*
+メ ${prefix}shortlink *link*
+メ ${prefix}chara *nino*
+メ ${prefix}cerpen
+メ ${prefix}ceritahoror
+メ ${prefix}jadibot
+メ ${prefix}stopjadibot
+メ ${prefix}listjadibot
+🂡⸻`
+}
+exports.groupmenu = (prefix) => 
+{
+return `⌜ 𝐆𝐑𝐎𝐔𝐏 𝐌𝐄𝐍𝐔 ⌝
+メ ${prefix}welcome *enable/disable*
+メ ${prefix}antilink *enable/disable*
+メ ${prefix}autosticker *1/0*
+メ ${prefix}tagall *teks*
+メ ${prefix}hidetag *teks*
+メ ${prefix}add *628××××××××*
+メ ${prefix}kick *@tag*
+メ ${prefix}promote *@tag*
+メ ${prefix}demote *@tag*
+メ ${prefix}setnamaa *nama*
+メ ${prefix}setppgc *reply*
+メ ${prefix}setdesc *teks*
+メ ${prefix}kontag *@tag|nama*
+メ ${prefix}kontak *@tag|nama*
+メ ${prefix}linkgc
+メ ${prefix}infogc
+メ ${prefix}listadmin
+メ ${prefix}listonline
+メ ${prefix}open
+メ ${prefix}close
+メ ${prefix}ganteng
+メ ${prefix}cantik
+メ ${prefix}jelek
+メ ${prefix}goblok
+メ ${prefix}bego
+メ ${prefix}pintar
+メ ${prefix}jago
+メ ${prefix}nolep
+メ ${prefix}monyet
+メ ${prefix}babi
+メ ${prefix}beban
+メ ${prefix}baik
+メ ${prefix}jahat
+メ ${prefix}anjing
+メ ${prefix}haram
+メ ${prefix}kontol
+メ ${prefix}pakboy
+メ ${prefix}pakgirl
+メ ${prefix}wibu
+メ ${prefix}hebat
+メ ${prefix}sadboy
+メ ${prefix}sadgirl
+🂡⸻`
+}
+exports.islamimenu = (prefix) => 
+{
+return `⌜ 𝐈𝐒𝐋𝐀𝐌𝐈 𝐌𝐄𝐍𝐔 ⌝
+メ ${prefix}asmaulhusna
+メ ${prefix}listsurah
+メ ${prefix}kisahnabi *nama nabi*
+メ ${prefix}jadwalsholat *nama kota*
+🂡⸻`
+}
+exports.downloadmenu = (prefix) => 
+{
+return `⌜ 𝐃𝐎𝐖𝐍𝐋𝐎𝐀𝐃 𝐌𝐄𝐍𝐔 ⌝
+メ ${prefix}play *quory*
+メ ${prefix}ytmp3 *link*
+メ ${prefix}ytmp4 *link*
+メ ${prefix}tiktok *link*
+メ ${prefix}tiktoknowm *link*
+メ ${prefix}tiktokwm *link*
+メ ${prefix}xnxxsearch *quory*
+メ ${prefix}xnxxdl *link*
+メ ${prefix}igvideo *link*
+メ ${prefix}igfoto *link*
+メ ${prefix}mediafire *link*
+メ ${prefix}tiktokstalk *username*
+メ ${prefix}githubstalk *username*
+メ ${prefix}instagramstalk *username*
+メ ${prefix}youtubestalk *username*
+🂡⸻`
+}
+exports.nulismenu = (prefix) => 
+{
+return `⌜ 𝐍𝐔𝐋𝐈𝐒 𝐌𝐄𝐍𝐔 ⌝
+メ ${prefix}nulis *teks*
+メ ${prefix}nuliskanan *teks*
+メ ${prefix}nuliskiri *teks*
+メ ${prefix}foliokanan *teks*
+メ ${prefix}foliokiri *teks*
+🂡⸻`
+}
+exports.stickermenu = (prefix) => 
+{
+return `⌜ 𝐒𝐓𝐈𝐂𝐊𝐄𝐑 𝐌𝐄𝐍𝐔 ⌝
+メ ${prefix}sticker *reply foto*
+メ ${prefix}toimg *reply sticker*
+メ ${prefix}tourl *reply foto*
+メ ${prefix}tts *teks*
+メ ${prefix}attp *teks*
+メ ${prefix}ttp *teks*
+メ ${prefix}ssweb *link*
+メ ${prefix}sswebfull *link*
+メ ${prefix}stickeranime
+メ ${prefix}gura
+メ ${prefix}patrick
+メ ${prefix}faktaunik
+メ ${prefix}katabijak
+メ ${prefix}pantun
+メ ${prefix}bucin
+メ ${prefix}puisi
+メ ${prefix}katailham
+メ ${prefix}katasindiran
+メ ${prefix}katabucin
+メ ${prefix}katabucin2
+メ ${prefix}kataml
+メ ${prefix}katagalau
+メ ${prefix}katagombal
+メ ${prefix}quotesislami
+メ ${prefix}quotesanime
+メ ${prefix}quotesdilan
+メ ${prefix}quotesff
+メ ${prefix}quotespubg
+メ ${prefix}quoteshacker
+メ ${prefix}truth
+メ ${prefix}dare
+🂡⸻`
+}
+exports.photoxymenu = (prefix) => 
+{
+return `⌜ 𝐏𝐇𝐎𝐓𝐎𝐗𝐘 𝐌𝐄𝐍𝐔 ⌝
+メ ${prefix}shadow *teks*
+メ ${prefix}cup *teks*
+メ ${prefix}cup1 *teks*
+メ ${prefix}romance *teks*
+メ ${prefix}smoke *teks*
+メ ${prefix}burnpaper *teks*
+メ ${prefix}lovemessage *teks*
+メ ${prefix}undergrass *teks*
+メ ${prefix}love *teks*
+メ ${prefix}coffe *teks*
+メ ${prefix}woodheart *teks*
+メ ${prefix}woodenboard *teks*
+メ ${prefix}summer3d *teks*
+メ ${prefix}wolfmetal *teks*
+メ ${prefix}nature3d *teks*
+メ ${prefix}underwater *teks*
+メ ${prefix}golderrose *teks*
+メ ${prefix}summernature *teks*
+メ ${prefix}lettesleaves *teks*
+メ ${prefix}glowingneon *teks*
+メ ${prefix}fallleaves *teks*
+メ ${prefix}flamming *teks*
+メ ${prefix}harrypotter *teks*
+メ ${prefix}carvedwood *teks*
+メ ${prefix}tiktokmaker *teks teks*
+メ ${prefix}arcade8bit *teks teks*
+メ ${prefix}battlefield4 *teks teks*
+メ ${prefix}pubg *teks teks*
+🂡⸻`
+}
+exports.ephotomenu = (prefix) => 
+{
+return `⌜ 𝐄𝐏𝐇𝐎𝐓𝐎 𝐌𝐄𝐍𝐔 ⌝
+メ ${prefix}wetglass *teks*
+メ ${prefix}multicolor3d *teks*
+メ ${prefix}watercolor *teks*
+メ ${prefix}luxurygold *teks*
+メ ${prefix}galaxywallpaper *teks*
+メ ${prefix}lighttext *teks*
+メ ${prefix}beautifulflower *teks*
+メ ${prefix}puppycute *teks*
+メ ${prefix}royaltext *teks*
+メ ${prefix}heartshaped *teks*
+メ ${prefix}galaxystyle *teks*
+メ ${prefix}hologram3d *teks*
+メ ${prefix}greenneon *teks*
+メ ${prefix}glossychrome *teks*
+メ ${prefix}greenbush *teks*
+メ ${prefix}metallogo *teks*
+メ ${prefix}noeltext *teks*
+メ ${prefix}glittergold *teks*
+メ ${prefix}textcake *teks*
+メ ${prefix}starsnight *teks*
+メ ${prefix}wooden3d *teks*
+メ ${prefix}textbyname *teks*
+メ ${prefix}writegalacy *teks*
+メ ${prefix}galaxybat *teks*
+メ ${prefix}snow3d *teks*
+メ ${prefix}birthdayday *teks*
+メ ${prefix}goldplaybutton *teks*
+メ ${prefix}silverplaybutton *teks*
+メ ${prefix}freefire *teks*
+メ ${prefix}neondevil *teks*
+メ ${prefix}3dshiny *teks*
+メ ${prefix}zombie3d *teks*
+メ ${prefix}wordgreen *teks*
+メ ${prefix}cloud *teks*
+メ ${prefix}metal *teks*
+メ ${prefix}neon *teks*
+メ ${prefix}graffitycolor *teks*
+メ ${prefix}lightgalaxy *teks*
+メ ${prefix}hotmetalic *teks*
+メ ${prefix}snake *teks*
+メ ${prefix}graffiti5 *teks*
+メ ${prefix}graffiti3 *teks*
+メ ${prefix}graffiti *teks*
+メ ${prefix}neon2 *teks*
+メ ${prefix}thunder *teks*
+メ ${prefix}startsnight *teks*
+メ ${prefix}cake *teks*
+メ ${prefix}writing halk *teks*
+メ ${prefix}birthdaycake *teks*
+メ ${prefix}3dhologram *teks*
+メ ${prefix}lighteffects *teks*
+メ ${prefix}greenbrush *teks*
+メ ${prefix}cakes *teks*
+メ ${prefix}starnight2 *teks*
+メ ${prefix}glowing *teks*
+メ ${prefix}blackpinkneon *teks*
+メ ${prefix}3dcrack *teks*
+メ ${prefix}3dunderwater *teks*
+メ ${prefix}bearlogo *teks*
+メ ${prefix}graffiti2 *teks*
+メ ${prefix}watercolot *teks*
+メ ${prefix}clouds *teks*
+メ ${prefix}pubgmascot *teks*
+メ ${prefix}summerbeach *teks*
+メ ${prefix}neonlight *teks*
+メ ${prefix}summerbeach2 *teks*
+メ ${prefix}glow *teks*
+メ ${prefix}galaxy *teks*
+メ ${prefix}artpapercut *teks*
+メ ${prefix}brokenglass *teks*
+🂡⸻`
+}
+exports.cecanmenu = (prefix) => 
+{
+return `⌜ 𝐂𝐄𝐂𝐀𝐍 𝐌𝐄𝐍𝐔 ⌝
+メ ${prefix}cecan
+メ ${prefix}cogan
+メ ${prefix}cecan2
+メ ${prefix}cogan2
+メ ${prefix}china
+メ ${prefix}malaysia
+メ ${prefix}korea
+メ ${prefix}japan
+メ ${prefix}indonesia
+メ ${prefix}thailand
+メ ${prefix}vietnam
+メ ${prefix}jeni
+メ ${prefix}jiso
+メ ${prefix}justina
+メ ${prefix}lisa
+メ ${prefix}rose
+メ ${prefix}ryujin
+🂡⸻`
+}
+exports.fotomenu = (prefix) => 
+{
+return `⌜ 𝐅𝐎𝐓𝐎 𝐌𝐄𝐍𝐔 ⌝
+メ ${prefix}darkjokes
+メ ${prefix}motor
+メ ${prefix}mobil
+メ ${prefix}memeindo
+メ ${prefix}renungan
+メ ${prefix}quotesimage
+メ ${prefix}katakataimage
+メ ${prefix}aesthetic
+メ ${prefix}yulibocil
+メ ${prefix}doraemon
+メ ${prefix}pokemon
+メ ${prefix}pentol
+メ ${prefix}tatasurya
+メ ${prefix}kartun
+メ ${prefix}anjing
+メ ${prefix}kucing
+メ ${prefix}satanic
+メ ${prefix}boneka
+メ ${prefix}wallpaperhacker
+メ ${prefix}wallpalerhacker2
+メ ${prefix}wallpaperharley
+メ ${prefix}wallpaperjoker
+メ ${prefix}wallpaperpubg
+メ ${prefix}wallpaperhp
+メ ${prefix}wallpaperhp2
+メ ${prefix}wallpaperkpop
+メ ${prefix}wallpaperblackpink
+メ ${prefix}wallpapergame
+メ ${prefix}naruto
+メ ${prefix}boruto
+メ ${prefix}nekonime
+メ ${prefix}akira
+メ ${prefix}akiyama
+メ ${prefix}ana
+メ ${prefix}asuna
+メ ${prefix}ayuzawa
+メ ${prefix}chico
+メ ${prefix}chitoge
+メ ${prefix}deidara
+メ ${prefix}eba
+メ ${prefix}elaina
+メ ${prefix}emilia
+メ ${prefix}erza
+メ ${prefix}gremory
+メ ${prefix}hestie
+メ ${prefix}hinata
+メ ${prefix}isuzu
+メ ${prefix}itachi
+メ ${prefix}itori
+メ ${prefix}kagura
+メ ${prefix}kakasih
+メ ${prefix}kaori
+メ ${prefix}kaneki
+メ ${prefix}kotori
+メ ${prefix}kurumi
+メ ${prefix}madara
+メ ${prefix}mukasa
+メ ${prefix}miku
+メ ${prefix}minato
+メ ${prefix}nezuko
+メ ${prefix}rize
+メ ${prefix}shina
+メ ${prefix}shinka
+メ ${prefix}shinomiya
+メ ${prefix}shizuka
+メ ${prefix}tejina
+メ ${prefix}toukachan
+メ ${prefix}tsunade
+メ ${prefix}yotsuba
+メ ${prefix}yuki
+メ ${prefix}yumeko
+メ ${prefix}fanart
+メ ${prefix}nino
+メ ${prefix}fox
+メ ${prefix}dog
+メ ${prefix}cat
+メ ${prefix}panda
+メ ${prefix}panda1
+メ ${prefix}bird
+メ ${prefix}koala
+メ ${prefix}kurama
+メ ${prefix}rimuru
+メ ${prefix}dazai
+メ ${prefix}eren
+メ ${prefix}titan
+メ ${prefix}lebi
+メ ${prefix}sakura
+メ ${prefix}neji
+メ ${prefix}inosuke
+メ ${prefix}jiraya
+メ ${prefix}kiba
+メ ${prefix}sarada
+メ ${prefix}sasuka
+メ ${prefix}obito
+メ ${prefix}tanjiro
+メ ${prefix}luffy
+メ ${prefix}zoro
+メ ${prefix}sanji
+メ ${prefix}gon
+メ ${prefix}killua
+メ ${prefix}sagiri
+メ ${prefix}zenitsu
+メ ${prefix}natsu
+メ ${prefix}genos
+メ ${prefix}saitama
+メ ${prefix}rem
+🂡⸻`
+}
+exports.videomenu = (prefix) => 
+{
+return `⌜ 𝐕𝐈𝐃𝐄𝐎 𝐌𝐄𝐍𝐔 ⌝
+メ ${prefix}bokep
+メ ${prefix}beatvn
+メ ${prefix}jedagjedugff
+メ ${prefix}jedagjedugml
+メ ${prefix}jedagjedugpubg
+メ ${prefix}storywa
+メ ${prefix}storygalau
+メ ${prefix}storyanime
+メ ${prefix}storyanime2
+メ ${prefix}storytruk
+メ ${prefix}storybus
+メ ${prefix}storysholawatan
+メ ${prefix}asupan
+メ ${prefix}asupan2
+メ ${prefix}asupanloli
+メ ${prefix}bocil
+メ ${prefix}rikagusriani
+メ ${prefix}santuy
+メ ${prefix}ukhty
+メ ${prefix}gheayubi
+メ ${prefix}nantalia
+🂡⸻`
+}
+exports.rpgmenu = (prefix) => 
+{
+return `⌜ 𝐑𝐏𝐆 𝐌𝐄𝐍𝐔 ⌝
+メ ${prefix}joinrpg
+メ ${prefix}quest
+メ ${prefix}mining
+メ ${prefix}mancing
+メ ${prefix}luckyday
+メ ${prefix}luckytime
+メ ${prefix}adventure
+メ ${prefix}inventori
+メ ${prefix}killsilme
+メ ${prefix}killgoblin
+メ ${prefix}killdevil
+メ ${prefix}killbehemoth
+メ ${prefix}killdemond
+メ ${prefix}killdemondking
+メ ${prefix}sellikan
+メ ${prefix}sellbesi
+メ ${prefix}sellemas
+メ ${prefix}selldiamond
+🂡⸻`
+}
+exports.nsfwmenu = (prefix) => 
+{
+return `⌜ 𝐍𝐒𝐅𝐖 𝐌𝐄𝐍𝐔 ⌝
+メ ${prefix}waifu
+メ ${prefix}loli
+メ ${prefix}husbu
+メ ${prefix}milf
+メ ${prefix}cosplay
+メ ${prefix}wallml
+メ ${prefix}art
+メ ${prefix}bts
+メ ${prefix}exo
+メ ${prefix}elf
+メ ${prefix}neko
+メ ${prefix}shota
+メ ${prefix}sagiri
+メ ${prefix}shinobu
+メ ${prefix}megumin
+メ ${prefix}wallnime
+メ ${prefix}chiisaihentai
+メ ${prefix}trap
+メ ${prefix}blowjob
+メ ${prefix}yaoi
+メ ${prefix}ecchi
+メ ${prefix}ahegao
+メ ${prefix}hololewd
+メ ${prefix}sideoppai
+メ ${prefix}animefeets
+メ ${prefix}animebooty
+メ ${prefix}animethighss
+メ ${prefix}hentaiparadise
+メ ${prefix}animearmpits
+メ ${prefix}hentaifemdom
+メ ${prefix}lewdanimegirls
+メ ${prefix}biganimetiddies
+メ ${prefix}animebellybutton
+メ ${prefix}hentai4everyone
+メ ${prefix}bj
+メ ${prefix}ero
+メ ${prefix}cum
+メ ${prefix}feet
+メ ${prefix}yuri
+メ ${prefix}lewd
+メ ${prefix}feed
+メ ${prefix}eron
+メ ${prefix}solo
+メ ${prefix}gasm
+メ ${prefix}poke
+メ ${prefix}anal
+メ ${prefix}holo
+メ ${prefix}tits
+メ ${prefix}kuni
+メ ${prefix}kiss
+メ ${prefix}erok
+メ ${prefix}smug
+メ ${prefix}baka
+メ ${prefix}solog
+メ ${prefix}feetg
+メ ${prefix}lewdk
+メ ${prefix}pussy
+メ ${prefix}femdom
+メ ${prefix}cuddle
+メ ${prefix}hentai
+メ ${prefix}eroyuri
+メ ${prefix}cum_jpg
+メ ${prefix}erofeet
+メ ${prefix}holoero
+メ ${prefix}classic
+メ ${prefix}erokemo
+メ ${prefix}fox_girl
+メ ${prefix}futanari
+メ ${prefix}lewdkemo
+メ ${prefix}wallpaper
+メ ${prefix}pussy_jpg
+メ ${prefix}kemonomimi
+メ ${prefix}nsfw_avatar
+🂡⸻`
+}
+exports.gamemenu = (prefix) => 
+{
+return `⌜ 𝐆𝐀𝐌𝐄 𝐌𝐄𝐍𝐔 ⌝
+メ ${prefix}suit
+メ ${prefix}bisakah
+メ ${prefix}kapankah
+メ ${prefix}jadian
+メ ${prefix}cantikcek
+メ ${prefix}gantengcek
+メ ${prefix}jelekcek
+メ ${prefix}goblokcek  
+メ ${prefix}begocek 
+メ ${prefix}pintarcek 
+メ ${prefix}jagocek 
+メ ${prefix}nolepcek 
+メ ${prefix}monyetcek     	 
+メ ${prefix}babicek 
+メ ${prefix}bebancek 
+メ ${prefix}baikcek 
+メ ${prefix}jahatcek 
+メ ${prefix}anjingcek 
+メ ${prefix}haramcek 
+メ ${prefix}kontolcek 
+メ ${prefix}pakboycek 
+メ ${prefix}pakgirlcek 
+メ ${prefix}wibucek 
+メ ${prefix}hebatcek 
+メ ${prefix}sadboycek 
+メ ${prefix}sadgirlcek`
+}
